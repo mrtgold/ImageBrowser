5e37c6e836477261ed6d06e1d707231104d0754a
mrtgold <timothy.golding@gmail.com>
2012-06-17 23:50:58 -0500

    minor cleanup

--- ImageBrowser/TestAsync/Form2.cs
+++ ImageBrowser/TestAsync/Form2.cs
@@ -10,44 +10,16 @@ namespace TestAsync
     public partial class Form2 : Form
     {
         private readonly ThumbnailSets _thumbnailSets;
-        private  Point _listViewLocation;
-        private  Size _listViewSize;
-        private  Control _listViewParent;
-        private readonly Process _proc;
+        private readonly Point _listViewLocation;
+        private readonly Size _listViewSize;
 
 
         public Form2()
         {
             InitializeComponent();
-            CaptureListViewParams(listView1);
-            _thumbnailSets = new ThumbnailSets(_listViewParent, InitializeListView, new[] { "*.jpg", "*.bmp", "*.png" });
-            _proc = Process.GetCurrentProcess();
-        }
-
-        private void UpdateStatusBar(string dir = null)
-        {
-            if (dir != null)
-                toolStripDirInfo.Text = dir;
-
-            var memoryUsed = GetMemoryUsed();
-            toolStripAppInfo.Text = memoryUsed;
-
-            toolStripImagesInfo.Text = " ";
-        }
-
-        private string GetMemoryUsed()
-        {
-            _proc.Refresh();
-            var privateBytes = _proc.PrivateMemorySize64;
-            var memoryUsed = string.Format("App PrivateBytes: {0:N0} KB", privateBytes/1024);
-            return memoryUsed;
-        }
-
-        private  void CaptureListViewParams(ListView listView)
-        {
-            _listViewParent = listView.Parent;
-            _listViewLocation = listView.Location;
-            _listViewSize = listView.Size;
+            _listViewLocation = listView1.Location;
+            _listViewSize = listView1.Size;
+            _thumbnailSets = new ThumbnailSets(listView1.Parent, InitializeListView, new[] { "*.jpg", "*.bmp", "*.png" });
         }
 
         private  void InitializeListView(ListView listView)
@@ -74,6 +46,31 @@ namespace TestAsync
             UpdateStatusBar(dir.FullName);
             _thumbnailSets.DisplayList(dir, ref listView1);
         }
+
+        private void UpdateStatusBar(string dir = null)
+        {
+            if (dir != null)
+                toolStripDirInfo.Text = dir;
+
+            var memoryUsed = GetMemoryUsed();
+            toolStripAppInfo.Text = memoryUsed;
+
+            toolStripImagesInfo.Text = " ";
+        }
+
+        private static string GetMemoryUsed()
+        {
+            long privateBytes;
+            using (var proc = Process.GetCurrentProcess())
+            {
+                proc.Refresh();
+                privateBytes = proc.PrivateMemorySize64;
+            }
+
+            var memoryUsed = string.Format("App PrivateBytes: {0:N0} KB", privateBytes / 1024);
+            return memoryUsed;
+        }
+
 
     }
 }
