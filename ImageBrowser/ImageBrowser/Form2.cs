fa585857d8390fe6f168b7f53cb92f05cbd2a774
mrtgold <timothy.golding@gmail.com>
2012-06-20 00:57:57 -0500

    cleanup, refactoring

--- ImageBrowser/ImageBrowser/Form2.cs
+++ ImageBrowser/ImageBrowser/Form2.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Diagnostics;
 using System.IO;
 using System.Windows.Forms;
 using DirectoryBrowser;
-using ImageBrowserLogic;
-using ImageBrowserLogic.ImageProviders;
-using ImageBrowserLogic.LoadingStrategies;
 using ImageBrowserPresenter;
 
 namespace ImageBrowser
@@ -21,33 +17,27 @@ namespace ImageBrowser
         {
             InitializeComponent();
             _presenter = new ImageBrowserPresenter2(this);
-             _listViewParentContainer = listView1.Parent;
-       }
-
-        public DirectoryTree DirectoryTree
-        {
-            get { return directoryTree1; }
+            _listViewParentContainer = listView1.Parent;
         }
+
 
         private void Form2_Load(object sender, EventArgs e)
         {
             if (BrowserViewLoad != null)
                 BrowserViewLoad(this, this);
-
-            //directoryTree1.InitDrives();
-            //directoryTree1.DirectorySelected += OnDirectorySelected;
         }
 
         public Control ListViewParentContainer { get { return _listViewParentContainer; } }
+        public DirectoryTree DirectoryTree { get { return directoryTree1; } }
 
         public void InitializeListView(ListView listView)
         {
-            listView.View = View.LargeIcon;
-            listView.LabelEdit = false;
-            listView.LabelWrap = true;
-            listView.Scrollable = true;
-            listView.Dock = DockStyle.Fill;
-            listView.UseCompatibleStateImageBehavior = false;
+            listView.View = listView1.View;
+            listView.LabelEdit = listView1.LabelEdit;
+            listView.LabelWrap = listView1.LabelWrap;
+            listView.Scrollable = listView1.Scrollable;
+            listView.Dock = listView1.Dock;
+            listView.UseCompatibleStateImageBehavior = listView1.UseCompatibleStateImageBehavior;
             listView.Location = listView1.Location;
             listView.Size = listView1.Size;
         }
@@ -55,13 +45,9 @@ namespace ImageBrowser
         public void OnDirectorySelected(DirectoryInfo dir)
         {
             if (DirectorySelected != null)
-                DirectorySelected(this,dir,ref listView1);
-            //UpdateStatusBar(dir.FullName);
-            //_thumbnailSets.DisplayList(dir, ref listView1);
-            //UpdateStatusBar(dir.FullName);
+                DirectorySelected(this, dir, ref listView1);
         }
 
-        #region StatusBar updates
         public void UpdateDirStatus(string msg)
         {
             toolStripDirInfo.Text = msg;
@@ -74,41 +60,6 @@ namespace ImageBrowser
         {
             toolStripImagesInfo.Text = msg;
         }
-        //private void UpdateStatusBar(string dir = null)
-        //{
-        //    if (dir != null)
-        //        toolStripDirInfo.Text = dir;
-
-        //    var memoryUsed = GetMemoryUsed();
-        //    toolStripAppInfo.Text = memoryUsed;
-
-        //    UpdateImageListCount();
-        //}
-
-        //private void UpdateImageListCount()
-        //{
-        //    if (listView1.LargeImageList != null)
-        //    {
-        //        var numLoaded = listView1.LargeImageList.Images.Count - 1;
-        //        var numFiles = listView1.Items.Count;
-        //        toolStripImagesInfo.Text = string.Format("{0} of {1} images loaded", numLoaded, numFiles);
-        //    }
-        //}
-
-        //private static string GetMemoryUsed()
-        //{
-        //    long privateBytes;
-        //    using (var proc = Process.GetCurrentProcess())
-        //    {
-        //        proc.Refresh();
-        //        privateBytes = proc.PrivateMemorySize64;
-        //    }
-
-        //    var memoryUsed = string.Format("App PrivateBytes: {0:N0} KB", privateBytes / 1024);
-        //    return memoryUsed;
-        //}
-
-        #endregion
 
     }
 }
